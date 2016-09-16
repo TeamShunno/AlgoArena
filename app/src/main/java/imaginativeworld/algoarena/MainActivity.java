709be6dc12c587b1dc2b1a4ea6c36e0b06ca96db
Shohag <imaginativeshohag@yahoo.com>
2016-09-16 06:09:40 +0600

    HTML updated. :)

--- app/src/main/java/imaginativeworld/algoarena/MainActivity.java
+++ app/src/main/java/imaginativeworld/algoarena/MainActivity.java
@@ -2,9 +2,6 @@ package imaginativeworld.algoarena;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -109,16 +106,13 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_dp) {
 
-        }
-        else if (id == R.id.nav_num) {
+        } else if (id == R.id.nav_num) {
 
-        }
-        else if (id == R.id.nav_string) {
+        } else if (id == R.id.nav_string) {
 
-        }
-        else if (id == R.id.nav_greedy) {
+        } else if (id == R.id.nav_greedy) {
 
-        }else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
